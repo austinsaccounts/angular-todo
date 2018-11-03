eb622906c7d2714842ed722aad53e6f65fc100d7
Austin C <austins.accts@gmail.com>
2018-11-02 18:19:22 -0700

    these will start a new folder/file for task.model,  will link accordingly with a export and import in compnent.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
   selector: 'crazy-app-selector',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-angular';
+  currentFocus: string = 'angular homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  tasks: Task[] = [
+    new Task('finish angular homework'),
+    new Task('brainstorm js group projects'),
+    new Task('add readme'),
+    new Task('add readme to angular repos')
 }
